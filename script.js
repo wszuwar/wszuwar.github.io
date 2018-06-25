a4bf59a416be26dbccd8efac635f1522172d70d9
“wszuwar <msuwr315@gmail.com>
2018-06-25 23:56:08 +0200

    "third commit"

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     const apiRoot = 'https://young-journey-52503.herokuapp.com/v1/task/';
-    const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+    const trelloApiRoot = 'https://young-journey-52503.herokuapp.com/v1/task/';
     const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     const $tasksContainer = $('[data-tasks-container]');
 
