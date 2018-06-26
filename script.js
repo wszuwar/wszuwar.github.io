d8d1856fc528e175bac6a8d3dddf1cbbf0589273
“wszuwar <msuwr315@gmail.com>
2018-06-26 19:00:22 +0200

    "script.js changee"

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    const apiRoot = 'https://young-journey-52503.herokuapp.com/v1/task/';
+    const apiRoot = 'https://young-journey-52503.herokuapp.com/';
     const trelloApiRoot = 'https://young-journey-52503.herokuapp.com/';
     const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     const $tasksContainer = $('[data-tasks-container]');
